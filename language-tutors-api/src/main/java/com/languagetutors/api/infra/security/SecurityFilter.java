32212c1a918e76380fd477ea0fc1c1be7be12c18
erick7amorim <erickamorimcarv@gmail.com>
2024-05-12 17:01:41 -0300

    security: criar filtro que recebe token de cada requisição

---
+++ language-tutors-api/src/main/java/com/languagetutors/api/infra/security/SecurityFilter.java
@@ -0,0 +1,29 @@
+package com.languagetutors.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class SecurityFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = recuperarToken(request);
+
+        System.out.println(tokenJWT);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader == null) {
+            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        }
+
+        return authorizationHeader.replace("Bearer ", "");
+    }
+}
